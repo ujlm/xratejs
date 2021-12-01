3faa51cc0ae52196541e08e7a2c6940be514fdfe
ujlm <maesulysse@gmail.com>
2021-12-01 23:31:56 +0100

    Changed to multiple destination currencies

--- README.md
+++ README.md
@@ -1,5 +1,8 @@
 # xratejs
-A simple solution to include the latest currency conversions in your HTML
+A simple solution to include the latest currency conversions in your HTML.
+You pass the portfolio as an array of currency_symbol-value pairs inside the from attribute.
+The exhange rates are fetched using the cryptocompare API cryptocompare.com.
+Consider using the API with your own API key: https://min-api.cryptocompare.com/pricing 
 
 # Dependencies
 Include the script in your html before closing body tag
@@ -8,6 +11,11 @@ Include the script in your html before closing body tag
 ```
 
 # Usage
+```html
+<span class="xratejs" from='[{"OFFICIAL_CURRENCY_SYMBOL": VALUE}, {"OFFICIAL_CURRENCY_SYMBOL": VALUE}, {...}]' to="OFFICIAL_CURRENCY_SYMBOL"></span>
+<!-- Pay attention to use double quotes for the object keys -->
+```
+# Examples
 ```html
 <!-- Basic conversion -->
 My $5 is worth €<span class="xratejs" from='[{"USD": 5}]' to="EUR"></span>
--- xrate.js
+++ xrate.js
@@ -1,9 +1,8 @@
 
 /*
-V1 of the currentWalletWidget: only limited list of exchange rates available
+V1.1 of xratejs
 */
-function getExchangeRate (to, callback){
-    var requestURL = `https://api.exchangerate.host/latest?base=${to}`; // Or should we use https://min-api.cryptocompare.com/data/price?fsym=EUR&tsyms=GBP https://min-api.cryptocompare.com/data/price?fsym=BTC&tsyms=USD,JPY,EUR
+function getExchangeRate (requestURL, callback){
     var request = new XMLHttpRequest();
     request.open('GET', requestURL);
     request.responseType = 'json';
@@ -14,31 +13,63 @@ function getExchangeRate (to, callback){
     }
 }
 
-function totalTo(valArr, xrate){
+function totalTo(to, valArr, xrate){
     var total = 0;
     valArr.forEach((item) => {
         var from = Object.keys(item)[0];
-        var rate = xrate.rates[from];
+        var rate = xrate[from];
         if(rate != 0){
             total += item[from]/rate; // TO: USD <--> FROM: BTC = 0.000018, so 4 BTC = (4 BTC / 0.000018)USD
         }
     });
+    total = total*xrate[to]; // Eg base is EUR (baseTo) but we want to convert to USD (to)
     return total.toFixed(2).toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
 }
 
 function setExchangeRate(xrate){
+    // This part can probably be optimized
     var io = document.getElementsByClassName("xratejs");
     for(var i = 0; i < io.length; i++) {
         (function(index) {
         valArr = JSON.parse(io[i].getAttribute('from'));
         to = io[i].getAttribute('to');
-        io[i].innerHTML = totalTo(valArr, xrate);
+        io[i].innerHTML = totalTo(to, valArr, xrate);
         })(i);
     }
 }
 
+function getKeyByValue(object, value) {
+    return Object.keys(object).find(key => object[key] === value);
+  }
+  
+
+function constructUrl(){
+    var to = [];
+    var from = [];
+    var io = document.getElementsByClassName("xratejs");
+    for(var i = 0; i < io.length; i++) {
+        (function(index) {
+        valArr = JSON.parse(io[i].getAttribute('from'));
+        valArr.forEach((item) => {
+            from.push(Object.keys(item)[0]);
+        });
+        // Add to to from as well
+        if(!from.includes(io[i].getAttribute('to'))){
+            from.push(io[i].getAttribute('to'));
+        }
+        if(!to.includes(io[i].getAttribute('to'))){
+            if(to.length < 1){
+                to.push(io[i].getAttribute('to'));
+            }
+        }
+        })(i);
+    }
+    var url = `https://min-api.cryptocompare.com/data/price?fsym=${to[0]}&tsyms=${from.toString()}`;
+    return url;
+}
+
 document.addEventListener("DOMContentLoaded", function() {
-    getExchangeRate('EUR', (xrate) => {
+    getExchangeRate(constructUrl(), (xrate) => {
         setExchangeRate(xrate);
     });
 });
